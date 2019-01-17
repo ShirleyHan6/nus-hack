7e2f79bef82409fe2277192fbad779fb83754dba
HAN SIMENG <hans0035@e.ntu.edu.sg>
2019-01-17 13:51:06 +0800

    update

--- process.py
+++ process.py
@@ -1,5 +1,15 @@
 import requests
 import json
+import re
+
+
+def find_vid(link):
+    match = re.search(r"youtube\.com/.*v=([^&]*)", link)
+    if match:
+        result = match.group(1)
+        return result
+    else:
+        return None
 
 
 def find_title(vid):
@@ -7,6 +17,5 @@ def find_title(vid):
                'key': "AIzaSyDwe_CX0WJV2aZH_k8xMcT4nSJxn9BD0n0"}
     data = requests.Session().get('https://www.googleapis.com/youtube/v3/videos', params=payload)
     resp_dict = json.loads(data.content)
-    print(resp_dict['items'][0]['snippet']['title'])
-
+    return resp_dict['items'][0]['snippet']['title']
 
