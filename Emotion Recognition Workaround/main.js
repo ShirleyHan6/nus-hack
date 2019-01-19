f365532cb6613e7380e4fcfa1b42efd0f5291895
Atrik Das <atrikdas@gmail.com>
2019-01-20 03:37:07 +0800

    update

--- Emotion Recognition Workaround/main.js
+++ Emotion Recognition Workaround/main.js
@@ -1,3 +1,26 @@
+$(document).ready(function(){
+  chrome.tabs.getSelected(null,function(tab) {
+    var tablink = document.createElement('a');
+    tablink.href = tab.url;
+    $('#host').html("Host : " + tablink)
+    });
+  // var timestamp = document.querySelector('.video-stream').getCurrentTime();
+  // $('#time').html("Current Time : " + timestamp);
+  jQuery(function(){
+   jQuery('#start').click();
+});
+});
+
+// Add listener for start button
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("start").addEventListener('click', process_video);
+});
+
+
+function process_video() {
+    chrome.tabs.executeScript(null, {file: 'test.js'});
+    document.getElementById("messages").innerHTML = lmao;
+}
 const mediaSource = new MediaSource();
 mediaSource.addEventListener('sourceopen', handleSourceOpen, false);
 let mediaRecorder;
--- Emotion Recognition Workaround/test.js
+++ Emotion Recognition Workaround/test.js
@@ -1,26 +1,3 @@
+    var lmao = document.getElementsByClassName('video-stream')[0].currentTime;
+	console.log(lmao);
 
-var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-var player;
-
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        events: {
-            'onReady': onPlayerReady
-        }
-    });
-}
-
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
-
-$('button').click(function() {
-  console.log(document.querySelector('.video-stream').getCurrentTime())
-})
